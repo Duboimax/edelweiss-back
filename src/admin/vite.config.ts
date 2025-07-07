ec32a9fc09fe45320e7984dfa4839a931ec7747d
Maxence DUBOIS <dubois.maxence@compilatio.net>
2025-07-07 14:21:37 +0200

    add vite config

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -1,15 +1,20 @@
 import { mergeConfig, type UserConfig } from 'vite';
 
 export default (config: UserConfig) => {
-  // Important: always return the modified config
   return mergeConfig(config, {
     resolve: {
       alias: {
         '@': '/src',
       },
-      server: {
-        allowedHosts: true,
+    },
+    server: {
+      host: true,
+      hmr: {
+        clientPort: 443,
       },
+    },
+    define: {
+      global: 'globalThis',
     },
   });
 };
